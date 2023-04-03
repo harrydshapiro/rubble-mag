8878f0ba94554a0424c565c7d9a1542ff2f5f76d
Harry Shapiro <50972797+harrydshapiro@users.noreply.github.com>
2023-04-02 20:49:49 -0400

    ready for review

--- src/features.tsx
+++ src/features.tsx
@@ -11,13 +11,13 @@ const loadingPoster = (<div id="poster" slot="poster">
 </div>)
 
 export const features: Record<string, { modelViewerElement: any, cameraOrbit: string, fieldOfView: string, title: string }> = {
-  books: {
-    title: 'books title',
-    cameraOrbit: "-440.6deg 22.5deg 2.138m",
-    fieldOfView: "30deg",
+  eyes_boxes: {
+    title: 'What are you looking at?',
+    cameraOrbit: "109.3deg 65.56deg 6.859m",
+    fieldOfView: "32.33deg",
     modelViewerElement: (
-      <model-viewer 
-        src="/features/books/render.glb"
+      <model-viewer
+        src="/features/eyes_boxes/render.glb"
         camera-controls
         shadow-intensity="1" 
         exposure="1" 
@@ -27,15 +27,15 @@ export const features: Record<string, { modelViewerElement: any, cameraOrbit: st
       >
         {loadingPoster}
       </model-viewer>
-      )  
+    )
   },
-  garbage: {
-    title: 'Stop staring at me...',
-    cameraOrbit: "55.77deg 86.92deg 5.488m",
-    fieldOfView: "30deg",
+  dogshit: {
+    title: 'Too many dogs in Williamsburg',
+    cameraOrbit: "159deg 50.65deg 1.975m",
+    fieldOfView: "33.65deg",
     modelViewerElement: (
-      <model-viewer 
-        src="/features/garbage/render.glb"
+      <model-viewer
+        src="/features/dogshit/render.glb"
         camera-controls
         shadow-intensity="1" 
         exposure="1" 
@@ -45,10 +45,28 @@ export const features: Record<string, { modelViewerElement: any, cameraOrbit: st
       >
         {loadingPoster}
       </model-viewer>
-    )  
+    )
+  },
+  toilet: {
+    title: 'Leave it in the Loo',
+    cameraOrbit: "55.31deg 80.93deg 4.017m",
+    fieldOfView: "30.62deg",
+    modelViewerElement: (
+      <model-viewer
+        src="/features/toilet/render.glb"
+        camera-controls
+        shadow-intensity="1" 
+        exposure="1" 
+        loading="lazy"
+        class="modelViewer"
+        interpolation-decay="80"
+      >
+        {loadingPoster}
+      </model-viewer>
+    )
   },
   rat: {
-    title: 'rat',
+    title: 'Forgot to look twice',
     cameraOrbit: "-0.3296deg 22.5deg 2.137m",
     fieldOfView: "30deg",
     modelViewerElement: (
@@ -66,7 +84,7 @@ export const features: Record<string, { modelViewerElement: any, cameraOrbit: st
     )  
   },
   champagne: {
-    title: 'champagne',
+    title: 'Congratulations',
     cameraOrbit: "-689.8deg 66.31deg 2.096m",
     fieldOfView: "19.85deg",
     modelViewerElement: (
@@ -83,31 +101,13 @@ export const features: Record<string, { modelViewerElement: any, cameraOrbit: st
       </model-viewer>
     )  
   },
-  "trash-bags": {
-    title: 'trash-bags',
-    cameraOrbit: "-817.8deg 31.69deg 5.28m",
-    fieldOfView: "22.96deg",
+  banana: {
+    title: 'Don\'t slip on me',
+    cameraOrbit: "-484.9deg 55.68deg 2.6245m",
+    fieldOfView: "30deg",
     modelViewerElement: (
       <model-viewer 
-        src="/features/trash-bags/render.glb"
-        camera-controls
-        shadow-intensity="1" 
-        exposure="1" 
-        loading="lazy"
-        class="modelViewer"
-        interpolation-decay="80"
-      >
-        {loadingPoster}
-      </model-viewer>
-    )  
-  },
-  "whole-foods": {
-    title: 'whole-foods',
-    cameraOrbit: "-811.2deg 22.5deg 3.076m",
-    fieldOfView: "22.96deg",
-    modelViewerElement: (
-      <model-viewer 
-        src="/features/trash-bags/render.glb"
+        src="/features/banana/render.glb"
         camera-controls
         shadow-intensity="1" 
         exposure="1" 
@@ -120,7 +120,7 @@ export const features: Record<string, { modelViewerElement: any, cameraOrbit: st
     )  
   },
   pizza: {
-    title: 'pizza',
+    title: '99 Cents. Fresh \& Hot',
     cameraOrbit: "-811.5deg 74.92deg 4.091m",
     fieldOfView: "20.67deg",
     modelViewerElement: (
@@ -137,13 +137,175 @@ export const features: Record<string, { modelViewerElement: any, cameraOrbit: st
       </model-viewer>
     )  
   },
-  banana: {
-    title: 'banana',
-    cameraOrbit: "-484.9deg 55.68deg 2.6245m",
+  couch: {
+    title: 'Couch on couch',
+    cameraOrbit: "1119deg 62.99deg 6.677m",
+    fieldOfView: "30deg",
+    modelViewerElement: (
+      <model-viewer
+        src="/features/couch/render.glb"
+        camera-controls
+        shadow-intensity="1" 
+        exposure="1" 
+        loading="lazy"
+        class="modelViewer"
+        interpolation-decay="80"
+      >
+        {loadingPoster}
+      </model-viewer>
+    )
+  },
+  bra: {
+    title: 'Free the nipple',
+    cameraOrbit: "367.4deg 49.66deg 1.427m",
+    fieldOfView: "33.63deg",
+    modelViewerElement: (
+      <model-viewer
+        src="/features/bra/render.glb"
+        camera-controls
+        shadow-intensity="1" 
+        exposure="1" 
+        loading="lazy"
+        class="modelViewer"
+        interpolation-decay="80"
+      >
+        {loadingPoster}
+      </model-viewer>
+    )
+  },
+  flower: {
+    title: 'Flower',
+    cameraOrbit: "737.3deg 37.31deg 0.4448m",
+    fieldOfView: "34.39deg",
+    modelViewerElement: (
+      <model-viewer
+        src="/features/flower/render.glb"
+        camera-controls
+        shadow-intensity="1" 
+        exposure="1" 
+        loading="lazy"
+        class="modelViewer"
+        interpolation-decay="80"
+      >
+        {loadingPoster}
+      </model-viewer>
+    )
+  },
+  books: {
+    title: "School's out",
+    cameraOrbit: "-440.6deg 22.5deg 2.138m",
     fieldOfView: "30deg",
     modelViewerElement: (
       <model-viewer 
-        src="/features/banana/render.glb"
+        src="/features/books/render.glb"
+        camera-controls
+        shadow-intensity="1" 
+        exposure="1" 
+        loading="lazy"
+        class="modelViewer"
+        interpolation-decay="80"
+      >
+        {loadingPoster}
+      </model-viewer>
+      )  
+  },
+  mayo: {
+    title: 'Has to be Heinz',
+    cameraOrbit: "1293deg 28.92deg 0.782m",
+    fieldOfView: "30deg",
+    modelViewerElement: (
+      <model-viewer
+        src="/features/mayo/render.glb"
+        camera-controls
+        shadow-intensity="1" 
+        exposure="1" 
+        loading="lazy"
+        class="modelViewer"
+        interpolation-decay="80"
+      >
+        {loadingPoster}
+      </model-viewer>
+    )
+  },
+  pasta: {
+    title: 'Tragedy after Lilia',
+    cameraOrbit: "607.8deg 68.38deg 1.104m",
+    fieldOfView: "32.81deg",
+    modelViewerElement: (
+      <model-viewer
+        src="/features/pasta/render.glb"
+        camera-controls
+        shadow-intensity="1" 
+        exposure="1" 
+        loading="lazy"
+        class="modelViewer"
+        interpolation-decay="80"
+      >
+        {loadingPoster}
+      </model-viewer>
+    )
+  },
+  pizza_beer: {
+    title: 'Dinner for 1',
+    cameraOrbit: "83.94deg 53.12deg 2.007m",
+    fieldOfView: "30.82deg",
+    modelViewerElement: (
+      <model-viewer
+        src="/features/pizza_beer/render.glb"
+        camera-controls
+        shadow-intensity="1" 
+        exposure="1" 
+        loading="lazy"
+        class="modelViewer"
+        interpolation-decay="80"
+      >
+        {loadingPoster}
+      </model-viewer>
+    )
+  },
+  trash_lots: {
+    title: 'Assortive Mating',
+    cameraOrbit: "2159deg 54.6deg 12.53m",
+    fieldOfView: "30deg",
+    modelViewerElement: (
+      <model-viewer
+        src="/features/trash_lots/render.glb"
+        camera-controls
+        shadow-intensity="1" 
+        exposure="1" 
+        loading="lazy"
+        class="modelViewer"
+        interpolation-decay="80"
+      >
+        {loadingPoster}
+      </model-viewer>
+    )
+  },
+  water: {
+    title: 'Sparkling or Still?',
+    cameraOrbit: "-51.36deg 61.02deg 2.037m",
+    fieldOfView: "34.09deg",
+    modelViewerElement: (
+      <model-viewer
+        src="/features/water/render.glb"
+        camera-controls
+        shadow-intensity="1" 
+        exposure="1" 
+        loading="lazy"
+        class="modelViewer"
+        interpolation-decay="80"
+      >
+        {loadingPoster}
+      </model-viewer>
+    )
+  },
+  "whole_foods": {
+    title: 'Housekeeping',
+    cameraOrbit: "-811.2deg 22.5deg 3.076m",
+    fieldOfView: "22.96deg",
+    modelViewerElement: (
+      <model-viewer 
+        src="/features/whole_foods/render.glb"
         camera-controls
         shadow-intensity="1" 
         exposure="1" 
@@ -154,5 +316,41 @@ export const features: Record<string, { modelViewerElement: any, cameraOrbit: st
         {loadingPoster}
       </model-viewer>
     )  
+  },
+  trashcan: {
+    title: 'Dont litter',
+    cameraOrbit: "1653deg 43.92deg 4.986m",
+    fieldOfView: "32.34deg",
+    modelViewerElement: (
+      <model-viewer
+        src="/features/trashcan/render.glb"
+        camera-controls
+        shadow-intensity="1" 
+        exposure="1" 
+        loading="lazy"
+        class="modelViewer"
+        interpolation-decay="80"
+      >
+        {loadingPoster}
+      </model-viewer>
+    )
+  },
+  sugar: {
+    title: 'Pour some sugar on me',
+    cameraOrbit: "-61.24deg 45.22deg 3.02m",
+    fieldOfView: "33.23deg",
+    modelViewerElement: (
+      <model-viewer
+        src="/features/sugar/render.glb"
+        camera-controls
+        shadow-intensity="1" 
+        exposure="1" 
+        loading="lazy"
+        class="modelViewer"
+        interpolation-decay="80"
+      >
+        {loadingPoster}
+      </model-viewer>
+    )
   }
 }
